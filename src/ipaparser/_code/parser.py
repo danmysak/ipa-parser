da479807a04ad69752b6026e8db74cf5b12cd77d
Danylo Mysak <danmysak@gmail.com>
2021-12-22 00:22:36 +0200

    Clearer docstring

--- src/ipaparser/_code/parser.py
+++ src/ipaparser/_code/parser.py
@@ -21,8 +21,8 @@ def parse_ipa(
     :param brackets: What to do with brackets in transcriptions denoting optional pronunciation, such as in
                      '[bə(j)ɪz⁽ʲ⁾ˈlʲivɨj]': expand ('[bəjɪzʲˈlʲivɨj]') or strip ('[bəɪzˈlʲivɨj]').
     :param combine: Two-sound sequences to be treated like they are always connected by a tie, e.g.,
-                    [('a', 'ɪ'), ('o', 'ʊ̯'), ('t', 's'), ('d̠', 'ɹ̠˔')]; note that the set of diacritics for each sound
-                    (in particular, no diacritics) will be matched exactly against the input transcription.
+                    [('a', 'ɪ'), ('o', 'ʊ̯'), ('t', 's'), ('d̠', 'ɹ̠˔')]; note that, e.g., ('a', 'ɪ') will not match
+                    'aɪ̯', and likewise ('a', 'ɪ̯') will not match 'aɪ'.
     :return: An IPA object corresponding to the (normalized) input string.
     :raises:
         TranscriptionEnclosingError: The input string is not properly enclosed in brackets (like [so] or /so/);
