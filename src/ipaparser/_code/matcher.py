3b41e0bb774749b69f8c6ddb9f9f8c6659e4720b
Danylo Mysak <danmysak@gmail.com>
2022-01-01 16:33:15 +0200

    Optimized time complexity for Matcher

--- src/ipaparser/_code/matcher.py
+++ src/ipaparser/_code/matcher.py
@@ -13,16 +13,8 @@ __all__ = [
 class Positions:
     positions: list[list[str]]
 
-    def __init__(self, base: Optional[list[list[str]]] = None) -> None:
-        self.positions = [] if base is None else base
-
-    def extend(self) -> Positions:
-        return Positions(self.positions + [[]])
-
-    def append(self, character: str) -> Positions:
-        if not self.positions:
-            raise ValueError('Cannot append to the last position of an empty list')
-        return Positions(self.positions[:-1] + [self.positions[-1] + [character]])
+    def __init__(self, data: list[list[str]]) -> None:
+        self.positions = data
 
     def string(self) -> str:
         return ''.join(''.join(position) for position in self.positions)
@@ -34,24 +26,43 @@ class Positions:
         return sum(map(len, self.positions))
 
 
+MatchBuilder = tuple[
+    Optional[bool],  # True = matched character, False = unmatched character, None = a new position
+    Optional['MatchBuilder'],  # preceding character, if any
+]
+
+
 class Match:
     match: Positions
     extra_diacritics: Positions
     original: Positions
 
-    def __init__(self, base: Optional[Match] = None, *, append: Optional[str] = None, matched: bool = True) -> None:
-        if base is None:
-            self.match = Positions()
-            self.extra_diacritics = Positions()
-            self.original = Positions()
-        elif append is None:
-            self.match = base.match.extend()
-            self.extra_diacritics = base.extra_diacritics.extend()
-            self.original = base.original.extend()
-        else:
-            self.match = base.match.append(append) if matched else base.match
-            self.extra_diacritics = base.extra_diacritics.append(append) if not matched else base.extra_diacritics
-            self.original = base.original.append(append)
+    def __init__(self, string: str, builder: Optional[MatchBuilder]) -> None:
+        match: list[list[str]] = []
+        extra_diacritics: list[list[str]] = []
+        original: list[list[str]] = []
+
+        def build(current: Optional[MatchBuilder]) -> int:  # Returns the number of matched characters
+            if current is None:
+                return 0
+            is_matched, nested = current
+            prefix_length = build(nested)
+            if is_matched is None:
+                match.append([])
+                extra_diacritics.append([])
+                original.append([])
+                return prefix_length
+            else:
+                updated = [original, match if is_matched else extra_diacritics]
+                assert all(updated) and len(string) > prefix_length
+                for updated_list in updated:
+                    updated_list[-1].append(string[prefix_length])
+                return prefix_length + 1
+
+        build(builder)
+        self.match = Positions(match)
+        self.extra_diacritics = Positions(extra_diacritics)
+        self.original = Positions(original)
 
     def position_count(self) -> int:
         return self.original.length()
@@ -61,7 +72,7 @@ class Match:
 
 
 Tree = dict[str, 'Tree']
-BranchesWithMatches = list[tuple[Tree, Match]]
+BranchesWithMatches = list[tuple[Tree, Optional[MatchBuilder]]]
 
 
 class Trie:
@@ -90,24 +101,24 @@ class Trie:
         self._make_leaf(node)
 
     def match(self, string: str, starting_at: int) -> list[Match]:
-        branches: BranchesWithMatches = [(self._tree, Match())]
+        branches: BranchesWithMatches = [(self._tree, None)]
         matches: list[Match] = []
         for position in count(starting_at):
             is_last = position == len(string)
             character = string[position] if not is_last else None
             combining_class = unicodedata.combining(character) if character else None
             if is_last or combining_class == 0:
-                matches.extend(match for branch, match in branches if self._is_leaf(branch))
+                matches.extend(Match(string, match) for branch, match in branches if self._is_leaf(branch))
             if is_last:
                 break
             if combining_class == 0 or position == starting_at:
-                branches = [(branch, Match(match)) for branch, match in branches]
+                branches = [(branch, (None, match)) for branch, match in branches]
             next_branches: BranchesWithMatches = []
             for branch, match in branches:
                 if character in branch:
-                    next_branches.append((branch[character], Match(match, append=character, matched=True)))
+                    next_branches.append((branch[character], (True, match)))
                 if combining_class != 0:
-                    next_branches.append((branch, Match(match, append=character, matched=False)))
+                    next_branches.append((branch, (False, match)))
             if not next_branches:
                 break
             branches = next_branches
