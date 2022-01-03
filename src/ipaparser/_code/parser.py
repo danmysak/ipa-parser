7e812edb02564addc2d5f4354bf968c75085d191
Danylo Mysak <danmysak@gmail.com>
2022-01-03 22:25:03 +0200

    Unknown symbols to be able to have components

--- src/ipaparser/_code/parser.py
+++ src/ipaparser/_code/parser.py
@@ -29,7 +29,7 @@ __all__ = [
 class Segment:
     start: int
     end: int
-    features: FeatureSet
+    features: Optional[FeatureSet] = None
     components: Optional[list[Segment]] = None
 
 
@@ -77,6 +77,8 @@ class Parser:
 
     def _expand(self, segment: Segment, min_position: int, max_position: int) -> Segment:
         features = segment.features
+        if features is None:
+            return segment
 
         def iterate(initial: int, step: int, final: int) -> int:
             nonlocal features
@@ -138,22 +140,22 @@ class Parser:
         result: list[Segment] = []
         for group in groups:
             if len(group) > 1:
-                if (combined := combine_features([segment.features for segment in group])) is not None:
-                    result.append(Segment(
-                        start=group[0].start,
-                        end=group[-1].end,
-                        features=combined,
-                        components=group,
-                    ))
+                feature_sets = [segment.features for segment in group]
+                result.append(Segment(
+                    start=group[0].start,
+                    end=group[-1].end,
+                    features=combine_features(feature_sets) if None not in feature_sets else None,
+                    components=group,
+                ))
             else:
                 result.append(group[0])
         return result
 
-    def _segment_to_symbol(self, segment: Segment) -> RawSymbol:
+    def _segment_to_symbol(self, segment: Segment, is_component: bool = False) -> RawSymbol:
         return RawSymbol(
-            string=self._extract(segment.start, segment.end, omit_final_tie=True),
+            string=self._extract(segment.start, segment.end, omit_final_tie=is_component),
             features=segment.features,
-            components=(list(map(self._segment_to_symbol, segment.components))
+            components=([self._segment_to_symbol(component, True) for component in segment.components]
                         if segment.components is not None else None),
         )
 
@@ -163,7 +165,7 @@ class Parser:
         start = 0
         next_segment = 0
         while start < self._total:
-            collected: list[Optional[Segment]] = []
+            collected: list[Segment] = []
             end = start
             while end < self._total and (end == start or self._is_tied(end - 1)):
                 if next_segment < len(segments) and end == segments[next_segment].start:
@@ -171,11 +173,14 @@ class Parser:
                     end = segments[next_segment].end
                     next_segment += 1
                 else:
-                    collected.append(None)
+                    collected.append(Segment(end, end + 1))
                     end += 1
-            if len(collected) == 1 and collected[0] and not self._is_tied(end - 1):
+            if len(collected) == 1 and not self._is_tied(end - 1):
                 symbols.append(self._segment_to_symbol(collected[0]))
             else:
-                symbols.append(RawSymbol(self._extract(start, end)))
+                symbols.append(RawSymbol(
+                    string=self._extract(start, end),
+                    components=[self._segment_to_symbol(segment, True) for segment in collected],
+                ))
             start = end
         return symbols
